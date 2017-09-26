6c846f3eaf11d81fbb31b17a4e5ba03bc4b73f6d
RamonRS <celmon1@gmail.com>
2017-09-26 14:57:34 +0200

    Ejemplos Variables

---
+++ 02 - Variables/08_LecturaPorTeclado.vb
@@ -0,0 +1,29 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        'Lectura de datos por teclado
+        Dim precioInicial, precioFinal As Double
+        Dim rebaja As Byte
+        Dim codigoVendedor As String
+        Console.WriteLine("Codigo vendedor:")
+        codigoVendedor = Console.ReadLine()
+
+        Console.WriteLine("Introduzca el precio del producto: ")
+        precioInicial = Convert.ToDouble(Console.ReadLine())
+
+        Console.WriteLine("Introduzca la rebaja que se debe aplicar: ")
+        rebaja = Convert.ToByte(Console.ReadLine())
+
+        precioFinal = precioInicial - (rebaja / 100) * precioInicial
+
+        Console.WriteLine("El precio final es " & precioFinal)
+        Console.ReadLine()
+
+
+
+
+
+    End Sub
+
+End Module
