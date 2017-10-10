88bffe983bd3f6d62cce89f5a06169468d379028
RamonRS <celmon1@gmail.com>
2017-10-10 15:00:26 +0200

    s

---
+++ 03 - If else/Boletin/Ejercicio5.vb
@@ -0,0 +1,25 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        Dim presion, temperatura As Double
+
+        presion = 1
+        temperatura = 300
+
+        If presion > 2 Then
+            Console.WriteLine("Reducir presión del núcleo")
+        End If
+
+        If temperatura > 500 Then
+            Console.WriteLine("Reducir temperatura")
+        End If
+
+        If presion < 2 And temperatura < 500 Then
+            Console.WriteLine("Todo en orden")
+        End If
+
+        Console.ReadLine()
+    End Sub
+
+End Module
